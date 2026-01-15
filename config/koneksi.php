3d9442c8500e3c466ed1240f3adc03a610bc2157
Randy DJamaludin <randy.djamaludin@nissinfoods.co.id>
2026-01-15 11:14:57 +0700

    Update database connection to use SSL

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,15 +1,12 @@
 <?php
-$server   = "nfi-psikotest-server.mysql.database.azure.com";
-$username = "ukyhzfyqwy";
-$password = "Nfi.600251@#!!!";
-$database = "psikotesonline";
-
-// Buat koneksi
-$koneksi = mysqli_connect($server, $username, $password, $database);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "{Nfi.600251@#!!!}", "{psikotesonline}", 3306, MYSQLI_CLIENT_SSL);
 
 // Cek koneksi
 if (!$koneksi) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 ?>
+
 
