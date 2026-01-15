41fbcde42e3893f998b630488ef2801903b1f022
Randy DJamaludin <randy.djamaludin@nissinfoods.co.id>
2026-01-15 11:24:08 +0700

    Enhance database connection with error handling
    Added error handling for database connection failure.

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,4 +3,7 @@ $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "{Nfi.600251@#!!!}", "{psikotesonline}", 3306, MYSQLI_CLIENT_SSL);
 
+if (!mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "PASSWORD_ANDA", "psikotesonline", 3306, MYSQLI_CLIENT_SSL)) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 ?>
+
