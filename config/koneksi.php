dd875a864589126de7d3ee9e7bc09cf501645eea
Randy DJamaludin <randy.djamaludin@nissinfoods.co.id>
2026-01-15 11:39:38 +0700

    Update MySQL connection setup in koneksi.php
    Refactor MySQL connection code to use variables and SSL.

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,12 +1,21 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "{Nfi.600251@#!!!}", "{psikotesonline}", 3306, MYSQLI_CLIENT_SSL);
+// 1. Definisikan variabel konfigurasi
+$host = "nfi-psikotest-server.mysql.database.azure.com";
+$user = "ukyhzfyqwy";
+$pass = "Nfi.600251@#!!!"; // Ganti dengan password yang benar
+$db   = "psikotesonline";
 
-if (!mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "Nfi.600251@#!!!", "psikotesonline", 3306, MYSQLI_CLIENT_SSL)) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// 2. Inisialisasi objek mysqli (Sangat Penting agar tidak NULL)
+$conn = mysqli_init();
+
+// 3. Atur SSL (Azure Flexible Server mewajibkan ini secara default)
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+// 4. Melakukan koneksi menggunakan mysqli_real_connect
+// Parameter: objek_koneksi, host, user, password, database, port, flag
+if (!mysqli_real_connect($conn, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL)) {
+    die("Gagal menyambung ke MySQL: " . mysqli_connect_error());
 }
+
+// Koneksi berhasil jika sampai di sini
 ?>
-
-
-
