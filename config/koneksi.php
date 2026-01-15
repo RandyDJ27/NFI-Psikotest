f20643c7d2ee6d2e78be509ca822127db2dd5c8f
Randy DJamaludin <randy.djamaludin@nissinfoods.co.id>
2026-01-15 13:49:42 +0700

    Refactor connection variable to $koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,17 +6,18 @@ $pass = "Nfi.600251@#!!!"; // Ganti dengan password yang benar
 $db   = "psikotestonline";
 
 // 2. Inisialisasi objek mysqli (Sangat Penting agar tidak NULL)
-$conn = mysqli_init();
+$koneksi = mysqli_init();
 
 // 3. Atur SSL (Azure Flexible Server mewajibkan ini secara default)
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 
 // 4. Melakukan koneksi menggunakan mysqli_real_connect
 // Parameter: objek_koneksi, host, user, password, database, port, flag
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($koneksi, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL)) {
     die("Gagal menyambung ke MySQL: " . mysqli_connect_error());
 }
 
 // Koneksi berhasil jika sampai di sini
 ?>
+
 
