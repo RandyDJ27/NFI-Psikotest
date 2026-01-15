11589e25f126f8657cd0eceb4a57f7eb72a51d9a
Randy DJamaludin <randy.djamaludin@nissinfoods.co.id>
2026-01-15 11:16:23 +0700

    Refactor database connection code in koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,10 +3,4 @@ $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($conn, "nfi-psikotest-server.mysql.database.azure.com", "ukyhzfyqwy", "{Nfi.600251@#!!!}", "{psikotesonline}", 3306, MYSQLI_CLIENT_SSL);
 
-// Cek koneksi
-if (!$koneksi) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
-}
 ?>
-
-
