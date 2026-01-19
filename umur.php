223cf80bf8e44b6100439bb949dffc722da677ad
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-19 16:48:10 +0700

    Update umur.php

--- umur.php
+++ umur.php
@@ -1,13 +1,10 @@
 <?php
-// 1. Tampilkan semua error agar tidak muncul 404 palsu
+// Tampilkan error biar kelihatan kalau ada masalah lain
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// 2. Sertakan koneksi
 include "config/koneksi.php";
 
-// 3. Fungsi hitung umur yang lebih stabil
 function hitung_umur($tgl) {
     $lahir = new DateTime($tgl);
     $hari_ini = new DateTime();
@@ -15,16 +12,11 @@ function hitung_umur($tgl) {
     return $diff->y;
 }
 
-// 4. Proses data hanya jika ada kiriman POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tgl_lahir'])) {
-    
     $tgl_lahir = $_POST['tgl_lahir'];
     $umur = hitung_umur($tgl_lahir);
 
-    // Cek kriteria umur (17 tahun ke atas)
     if ($umur >= 17) {
-        
-        // Bersihkan input agar query tidak rusak/error
         $username = mysqli_real_escape_string($koneksi, $_POST['username']);
         $password = md5($_POST['password']);
         $nama     = mysqli_real_escape_string($koneksi, $_POST['nama']);
@@ -33,27 +25,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['tgl_lahir'])) {
         $telp     = mysqli_real_escape_string($koneksi, $_POST['telp']);
         $alamat   = mysqli_real_escape_string($koneksi, $_POST['alamat']);
 
-        // Query INSERT yang lebih standar
-        $simpan = "INSERT INTO tbl_user (username, password, nama, tgl_lahir, jk, email, telp, alamat) 
-                   VALUES ('$username', '$password', '$nama', '$tgl_lahir', '$jk', '$email', '$telp', '$alamat')";
+        // TAMBAHKAN kolom stat_tes di sini, kita kasih nilai default 'Belum'
+        $simpan = "INSERT INTO tbl_user (username, password, nama, tgl_lahir, jk, email, telp, alamat, stat_tes) 
+                   VALUES ('$username', '$password', '$nama', '$tgl_lahir', '$jk', '$email', '$telp', '$alamat', 'Belum')";
         
         if (mysqli_query($koneksi, $simpan)) {
             echo '<script>alert("Anda Berhasil Melakukan Registrasi"); window.location="index.php";</script>';
         } else {
-            // Jika database error, pesan ini akan muncul (bukan 404 lagi)
-            echo "<h3>Gagal Simpan ke Database!</h3>";
-            echo "Pesan Error: " . mysqli_error($koneksi);
-            echo "<br><br><a href='pendaftaran.php'>Kembali ke Pendaftaran</a>";
+            die("Gagal simpan: " . mysqli_error($koneksi));
         }
-
     } else {
-        // Jika umur di bawah 17
         echo '<script>alert("Registrasi Gagal! Umur Anda Belum 17 Tahun"); window.location="pendaftaran.php";</script>';
     }
-
-} else {
-    // Jika file dibuka langsung tanpa isi form, balikkan ke pendaftaran
-    header("Location: pendaftaran.php");
-    exit();
 }
 ?>
