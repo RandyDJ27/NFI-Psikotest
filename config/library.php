1be6e3651798a8bea5b95e1cb387c4faccf1057a
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-20 14:31:37 +0700

    Update library.php

--- config/library.php
+++ config/library.php
@@ -1,8 +1,12 @@
 <?php
 require_once __DIR__ . '/koneksi.php';
 
-date_default_timezone_set('Asia/Jakarta'); // PHP 6 mengharuskan penyebutan timezone.
-$seminggu = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
+date_default_timezone_set('Asia/Jakarta');
+
+$seminggu = array(
+    "Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"
+);
+
 $hari = date("w");
 $hari_ini = $seminggu[$hari];
 
@@ -12,10 +16,17 @@ $bln_sekarang = date("m");
 $thn_sekarang = date("Y");
 $jam_sekarang = date("H:i:s");
 
-$nama_bln=array(1=> "Januari", "Februari", "Maret", "April", "Mei", 
-                    "Juni", "Juli", "Agustus", "September", 
-                    "Oktober", "November", "Desember");
-];
-
-
-
+$nama_bln = array(
+    1 => "Januari",
+    2 => "Februari",
+    3 => "Maret",
+    4 => "April",
+    5 => "Mei",
+    6 => "Juni",
+    7 => "Juli",
+    8 => "Agustus",
+    9 => "September",
+    10 => "Oktober",
+    11 => "November",
+    12 => "Desember"
+);
