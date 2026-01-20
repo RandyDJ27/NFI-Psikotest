79f965bfe3dc887e9ff2038b9fb1e3cd41c5b9fd
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-20 09:07:22 +0700

    Update content.php

--- admin/content.php
+++ admin/content.php
@@ -1,53 +1,53 @@
 <?php
-include "../config/koneksi.php";
-include "../config/library.php";
-include "../config/fungsi_indotgl.php";
+// Gunakan include_once agar tidak terjadi error jika file sudah dipanggil di media.php
+include_once "../config/koneksi.php";
+include_once "../config/library.php";
+include_once "../config/fungsi_indotgl.php";
 
 if (isset($_GET['module'])) {
     $module = $_GET['module'];
 
     switch ($module) {
         case 'home':
-            include "sidebar.php";
-            include "modul/mod_home/home";
+            // Cek apakah file home.php benar-benar ada di folder tersebut
+            $file = "modul/mod_home/home.php";
+            if (file_exists($file)) {
+                include $file;
+            } else {
+                echo "<p>Error: File <b>$file</b> tidak ditemukan di server Azure.</p>";
+            }
             break;
 
         case 'soal':
-            include "sidebar.php";
             include "modul/mod_soal/soal.php";
             break;
 
         case 'hasiltes':
-            include "sidebar.php";
             include "modul/mod_hasiltes/hasiltes.php";
             break;
 
         case 'pengaturantes':
-            include "sidebar.php";
             include "modul/mod_pengaturantes/pengaturantes.php";
             break;
 
         case 'users':
-            include "sidebar.php";
             include "modul/mod_users/users.php";
             break;
 
         case 'pengguna':
-            include "sidebar.php";
             include "modul/mod_pengguna/pengguna.php";
             break;
 
         case 'tentang':
-            include "sidebar.php";
             include "modul/mod_tentang/tentang.php";
             break;
 
         default:
-            echo "<p>404 Halaman tidak ditemukan.</p>";
+            echo "<p>404 Halaman modul <b>$module</b> tidak ditemukan.</p>";
             break;
     }
 } else {
-    header("Location: media.php?module=home");
+    // Arahkan ke media.php dengan parameter module
+    echo "<script>window.location='media.php?module=home';</script>";
 }
 ?>
-
