4379cec5e8cbb120c9297746177cb6de0eab8b36
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-21 08:51:25 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -16,4 +16,10 @@ mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 if (!mysqli_real_connect($koneksi, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL)) {
     die("Gagal menyambung ke MySQL: " . mysqli_connect_error());
 }
+
+// 5. set charset utf8mb4
+mysqli_set_charset($koneksi, "utf8mb4");
+mysqli_query($koneksi, "SET NAMES utf8mb4");
+mysqli_query($koneksi, "SET CHARACTER SET utf8mb4");
+
 ?>
