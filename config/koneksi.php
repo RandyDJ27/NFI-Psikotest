05ce6c08ef1fd2add1080cecc51028b5236e1316
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-21 09:20:47 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -8,6 +8,9 @@ $db   = "psikotestonline";
 // 2. Inisialisasi objek mysqli (Sangat Penting agar tidak NULL)
 $koneksi = mysqli_init();
 
+/* WAJIB: SET charset & collation SEBELUM connect */
+mysqli_options($koneksi, MYSQLI_INIT_COMMAND, "SET NAMES utf8mb4 COLLATE utf8mb4_0900_ai_ci");
+
 // 3. Atur SSL (Azure Flexible Server mewajibkan ini secara default)
 mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 
@@ -19,7 +22,6 @@ if (!mysqli_real_connect($koneksi, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT
 
 // 5. set charset utf8mb4
 mysqli_set_charset($koneksi, "utf8mb4");
-mysqli_query($koneksi, "SET NAMES utf8mb4");
-mysqli_query($koneksi, "SET CHARACTER SET utf8mb4");
 
 ?>
+
