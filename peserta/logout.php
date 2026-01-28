bf143b7f1a004d0bbe8e8bb4824af63bd83ef6b2
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-28 08:53:54 +0700

    Update logout.php

--- peserta/logout.php
+++ peserta/logout.php
@@ -4,9 +4,10 @@ session_start();
 session_destroy();
 	echo '<script language="javascript">
 	alert("Terima kasih . Anda Berhasil Logout");
-	window.location="index.php";
+	window.location="login.php";
 	</script>';
 
 
 ?>
+
 
