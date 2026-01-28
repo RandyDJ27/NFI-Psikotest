dd9e946c0e442cf2a3ba0d87fad445bc03e12bfa
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-01-28 09:02:03 +0700

    Update profileuser.php

--- peserta/profileuser.php
+++ peserta/profileuser.php
@@ -22,7 +22,7 @@
           <span class="menu-collapsed">Soal</span>
         </div>
       </a> -->
-      <a href="http://localhost/psikotes/peserta/logout.php"
+      <a href="logout.php"
         class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-start align-items-center">
           <span class="fa fa-sign-out-alt fa-fw mr-3"></span>
@@ -143,4 +143,5 @@
     agama '];?>');
   $('#kwgn').val('<?php echo $t['
     kwgn '];?>');
+
 </script>
