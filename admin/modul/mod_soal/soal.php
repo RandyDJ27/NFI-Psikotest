82b23900eb473f42cc3bc4c3468f4c6acb170c5b
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-02-07 07:23:50 +0700

    Update soal.php

--- admin/modul/mod_soal/soal.php
+++ admin/modul/mod_soal/soal.php
@@ -48,7 +48,7 @@
           <span class="menu-collapsed">Tentang</span>
         </div>
       </a>
-      <a href="http://localhost/psikotes/admin/logout.php"
+      <a href=logout.php"
         class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-start align-items-center">
           <span class="fa fa-sign-out-alt fa-fw mr-3"></span>
@@ -89,7 +89,7 @@ if (session_status() == PHP_SESSION_NONE) {
   echo "<a href=../../index.php><b>LOGIN</b></a></center>";
 }
 else{
-$aksi = "/admin/modul/mod_soal/aksi_soal.php";
+$aksi = "admin/modul/mod_soal/aksi_soal.php";
 $act = isset($_GET['act']) ? $_GET['act'] : '';
 switch ($act) {    
 // switch($_GET['act']){
@@ -383,4 +383,5 @@ switch ($act) {
   </script>
 </div>
 </div>
+
 
