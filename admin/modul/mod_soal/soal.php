470207bbc59d7fd0723e9cef00eb435c15adc6b8
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-02-07 07:36:34 +0700

    Update soal.php

--- admin/modul/mod_soal/soal.php
+++ admin/modul/mod_soal/soal.php
@@ -89,7 +89,7 @@ if (session_status() == PHP_SESSION_NONE) {
   echo "<a href=../../index.php><b>LOGIN</b></a></center>";
 }
 else{
-$aksi = "admin/modul/mod_soal/aksi_soal.php";
+$aksi = "modul/mod_soal/aksi_soal.php";
 $act = isset($_GET['act']) ? $_GET['act'] : '';
 switch ($act) {    
 // switch($_GET['act']){
@@ -383,5 +383,6 @@ switch ($act) {
   </script>
 </div>
 </div>
+
 
 
