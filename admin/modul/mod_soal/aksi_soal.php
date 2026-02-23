febb541a8f54be83fb07d54c157676991d99cc59
VieqriSamputra <vieqri.samputra.pratama@nissinfoods.co.id>
2026-02-23 09:06:40 +0700

    Update aksi_soal.php

--- admin/modul/mod_soal/aksi_soal.php
+++ admin/modul/mod_soal/aksi_soal.php
@@ -1,28 +1,27 @@
 <?php
-// 1. Pastikan tag pembuka PHP ada di Baris 1 Kolom 1
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-<?php
-session_start();
-// Baris untuk ngecek isi session (Hapus kalau sudah normal)
-// die(print_r($_SESSION)); 
-
-if (empty($_SESSION['username'])){
-    // ... sisa kodingan login ...
+// Cek apakah session ada. Kalau kosong, arahkan ke login.
+if (empty($_SESSION['username']) AND empty($_SESSION['passuser'])){
+    echo "<link href='style.css' rel='stylesheet' type='text/css'>
+    <center>Untuk mengakses modul, Anda harus login <br>";
+    // Link login saya perbaiki ke folder utama
+    echo "<a href='../../../index.php'><b>LOGIN</b></a></center>";
+    exit; // Menghentikan script agar tidak lanjut ke bawah
+}
 else{
     include "../../../config/koneksi.php";
     include "../../../config/library.php";
     include "../../../config/fungsi_thumb.php";
 
-    $module = $_GET['module'];
-    $act = $_GET['act'];
+    $module = isset($_GET['module']) ? $_GET['module'] : '';
+    $act    = isset($_GET['act']) ? $_GET['act'] : '';
 
-    // Input soal
+    // --- PROSES INPUT SOAL ---
     if ($module=='soal' AND $act=='input'){
         $lokasi_file    = $_FILES['fupload']['tmp_name'];
-        $tipe_file      = $_FILES['fupload']['type'];
         $nama_file      = $_FILES['fupload']['name'];
         $acak           = rand(1,99);
         $nama_file_unik = $acak.$nama_file;
@@ -31,19 +30,20 @@ else{
             UploadBanner($nama_file_unik);
             mysqli_query($koneksi, "INSERT INTO tbl_soal(soal,a,b,c,d,knc_jawaban,tanggal,gambar) 
                             VALUES('$_POST[soal]', '$_POST[a]', '$_POST[b]', '$_POST[c]', '$_POST[d]', '$_POST[knc_jawaban]', '$tgl_sekarang', '$nama_file_unik')");
-        }
-        else{
+        } else {
             mysqli_query($koneksi, "INSERT INTO tbl_soal(soal,a,b,c,d,knc_jawaban) 
                             VALUES('$_POST[soal]', '$_POST[a]', '$_POST[b]', '$_POST[c]', '$_POST[d]', '$_POST[knc_jawaban]')");
         }
         header('location:../../media.php?module='.$module);
     }
-    // Hapus Soal
+
+    // --- PROSES HAPUS SOAL ---
     elseif ($module=='soal' AND $act=='hapus') {
         mysqli_query($koneksi, "DELETE FROM tbl_soal WHERE id_soal='$_GET[id]'");
         header('location:../../media.php?module='.$module);
     }
-    // Update soal
+
+    // --- PROSES UPDATE SOAL ---
     elseif ($module=='soal' AND $act=='update'){
         $lokasi_file    = $_FILES['fupload']['tmp_name'];
         $nama_file      = $_FILES['fupload']['name'];
@@ -51,25 +51,11 @@ else{
         $nama_file_unik = $acak.$nama_file; 
 
         if (empty($lokasi_file)){
-            // Catatan: Saya ubah mysql_query menjadi mysqli_query agar sesuai dengan koneksi kamu
             mysqli_query($koneksi, "UPDATE tbl_soal SET soal = '$_POST[soal]', a = '$_POST[a]', b = '$_POST[b]', c = '$_POST[c]', d = '$_POST[d]', knc_jawaban = '$_POST[knc_jawaban]' WHERE id_soal = '$_POST[id]'");
-        }
-        else{
+        } else {
             UploadBanner($nama_file_unik);
             mysqli_query($koneksi, "UPDATE tbl_soal SET soal = '$_POST[soal]', a = '$_POST[a]', b = '$_POST[b]', c = '$_POST[c]', d = '$_POST[d]', knc_jawaban = '$_POST[knc_jawaban]', gambar = '$nama_file_unik' WHERE id_soal = '$_POST[id]'");
         }
         header('location:../../media.php?module='.$module);
     }
-    // Pengaktifan dan Pengnonaktifan
-    elseif ($module=='soal' AND $act=='nonaktif'){
-        $aktif='N';
-        mysqli_query($koneksi, "UPDATE tbl_soal SET aktif = '$aktif' WHERE id_soal='$_GET[id]'");
-        header('location:../../media.php?module='.$module);
-    }
-    elseif ($module=='soal' AND $act=='aktif'){
-        $aktif='Y';
-        mysqli_query($koneksi, "UPDATE tbl_soal SET aktif = '$aktif' WHERE id_soal='$_GET[id]'");
-        header('location:../../media.php?module='.$module);
-    }
 }
-// Jangan tambahkan tag penutup ?> di akhir file jika isinya murni PHP untuk menghindari spasi tak sengaja.
